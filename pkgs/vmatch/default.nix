c71131bc617e1b88ee1be72b73de468cf22a7028
anttiharju <anttiharju@users.noreply.github.com>
2025-10-27 19:10:45 +0000

    Update vmatch to v1.0.37
    https://github.com/anttiharju/vmatch/blob/3c06e33fd181c8af41a731156ba445afcae60154/.github/workflows/package.yml

--- pkgs/vmatch/default.nix
+++ pkgs/vmatch/default.nix
@@ -2,21 +2,21 @@
 
 buildGoModule {
   pname = "vmatch";
-  version = "1.0.36";
+  version = "1.0.37";
 
   src = fetchFromGitHub {
     owner = "anttiharju";
     repo = "vmatch";
-    rev = "d54e5aa02d01225a6e87d2ad8e56a207c65befa5";
-    hash = "sha256-HIjBi7EPnTNKHdiXfEvdEWeKh5ZPdFrjPtjZk2rPQNI=";
+    rev = "3c06e33fd181c8af41a731156ba445afcae60154";
+    hash = "sha256-uenFqlAw2AE/T2OLw69u7Mx8CtYfrJzkijeaczTpqm8=";
   };
 
   ldflags = [
     "-s"
     "-w"
-    "-X main.revision=d54e5aa02d01225a6e87d2ad8e56a207c65befa5"
-    "-X main.version=1.0.36"
-    "-X main.time=2025-10-27T18:52:53Z"
+    "-X main.revision=3c06e33fd181c8af41a731156ba445afcae60154"
+    "-X main.version=1.0.37"
+    "-X main.time=2025-10-27T19:07:59Z"
   ];
 
   vendorHash = null;
