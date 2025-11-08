b897132ecf40655281845a9faf2cae713419f9fa
anttiharju <anttiharju@users.noreply.github.com>
2025-11-08 15:40:15 +0000

    Update relcheck to 1.8.8
    https://github.com/anttiharju/relcheck/blob/1e5ce0de0faa8b1a99d58d3ffc6a2c2f6eac6009/.github/workflows/release.yml

--- pkgs/relcheck/default.nix
+++ pkgs/relcheck/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule {
   pname = "relcheck";
-  version = "1.8.7";
+  version = "1.8.8";
 
   src = fetchFromGitHub {
     owner = "anttiharju";
     repo = "relcheck";
-    rev = "5a5076a4fecebc0a181beec646da179e79b4841c";
-    hash = "sha256-NuikzbfKvmRmZjb9Z9Clk+c/LhOOPfEriVRdWPz7gCM=";
+    rev = "1e5ce0de0faa8b1a99d58d3ffc6a2c2f6eac6009";
+    hash = "sha256-UolI2pJAjwZFmlcnEh2Uml76EVezKx5UluYP3n9xSh8=";
   };
 
   buildInputs = [ git ];
@@ -16,9 +16,9 @@ buildGoModule {
   ldflags = [
     "-s"
     "-w"
-    "-X main.revision=5a5076a4fecebc0a181beec646da179e79b4841c"
-    "-X main.version=1.8.7"
-    "-X main.time=2025-11-08T13:18:23Z"
+    "-X main.revision=1e5ce0de0faa8b1a99d58d3ffc6a2c2f6eac6009"
+    "-X main.version=1.8.8"
+    "-X main.time=2025-11-08T15:38:18Z"
   ];
 
   vendorHash = null;
