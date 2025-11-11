8439534bdffba891bd3db2260c5a498b18889f82
Antti Harju <antti@harju.io>
2025-11-11 20:16:58 +0200

    Add an overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,16 @@
-{
-  # Add your overlays here
-  #
-  # my-overlay = import ./my-overlay;
-}
+final: prev:
+let
+  pkgsDir = ../pkgs;
+  packageNames = builtins.attrNames (builtins.readDir pkgsDir);
+
+  isDirectory = name: (builtins.readDir pkgsDir).${name} == "directory";
+  packageDirs = builtins.filter isDirectory packageNames;
+
+  packages = builtins.listToAttrs (
+    map (name: {
+      inherit name;
+      value = final.callPackage (pkgsDir + "/${name}") { };
+    }) packageDirs
+  );
+in
+packages
