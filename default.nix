a3341ee66f756fe6f98875cc2600699bbd327a98
Antti Harju <antti@harju.io>
2025-11-11 20:22:25 +0200

    More autodiscovery

--- default.nix
+++ default.nix
@@ -1,21 +1,23 @@
-# This file describes your repository contents.
-# It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
-# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
-# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
-# commands such as:
-#     nix-build -A mypackage
-
 { pkgs ? import <nixpkgs> { } }:
 
 let
   lib = import ./lib { inherit pkgs; };
+
+  # Auto-discover packages
+  pkgsDir = ./pkgs;
+  packageNames = builtins.attrNames (builtins.readDir pkgsDir);
+  isDirectory = name: (builtins.readDir pkgsDir).${name} == "directory";
+  packageDirs = builtins.filter isDirectory packageNames;
+
+  packages = builtins.listToAttrs (
+    map (name: {
+      inherit name;
+      value = pkgs.callPackage (pkgsDir + "/${name}") { inherit lib; };
+    }) packageDirs
+  );
 in
 {
   lib = lib;
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
-
-  relcheck = pkgs.callPackage ./pkgs/relcheck { inherit lib; };
-  vmatch = pkgs.callPackage ./pkgs/vmatch { inherit lib; };
-}
+} // packages
