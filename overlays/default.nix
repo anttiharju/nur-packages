61eb446667ca4576c2ecb3c944047fd04ac190bb
Antti Harju <antti@harju.io>
2025-11-11 20:21:22 +0200

    Import the custom lib

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,10 +6,12 @@ let
   isDirectory = name: (builtins.readDir pkgsDir).${name} == "directory";
   packageDirs = builtins.filter isDirectory packageNames;
 
+  lib = import ../lib { pkgs = final; };
+
   packages = builtins.listToAttrs (
     map (name: {
       inherit name;
-      value = final.callPackage (pkgsDir + "/${name}") { };
+      value = final.callPackage (pkgsDir + "/${name}") { inherit lib; };
     }) packageDirs
   );
 in
